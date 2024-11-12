07fecf7862efe99dfed21b141f2fe0a3880d0a41
Jakub Psarski <psarski@student.agh.edu.pl>
2024-11-12 16:33:21 +0100

    improvements to home page

--- microproject/script.js
+++ microproject/script.js
@@ -20,6 +20,26 @@ function showTime() {
 
 showTime();
 
+/* FAQ */
+var q = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < q.length; i++) {
+    q[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 /* Form */
 document.getElementById('contactForm').addEventListener('submit', function (e) {
     e.preventDefault();
