c60b597c19c6710117bda5b19766e05dada28bb9
Jakub Psarski <psarski@student.agh.edu.pl>
2024-11-12 01:23:26 +0100

    first draft

---
+++ microproject/script.js
@@ -0,0 +1,28 @@
+/* Theme */
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+}
+
+window.onload = () => {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+    }
+};
+
+/* Clock */
+
+setInterval(showTime, 1000);
+
+function showTime() {
+    let now = new Date();
+    document.getElementById("clock").textContent = now.toLocaleTimeString();
+}
+
+showTime();
+
+/* Form */
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Formularz został pomyślnie wysłany!');
+});
